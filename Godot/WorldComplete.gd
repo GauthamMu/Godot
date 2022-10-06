bfe8306254a304f3f9953b8ae30076f3ab10d43f
GauthamMu <100410553+GauthamMu@users.noreply.github.com>
2022-10-06 15:04:14 +0200

    Add files via upload

--- Godot/WorldComplete.gd
+++ Godot/WorldComplete.gd
@@ -1,6 +1,6 @@
 extends Area2D
 export(String, FILE, "*.tscn") var next_world
-func _physics_process(delta):
+func _physics_process(_delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Player":
